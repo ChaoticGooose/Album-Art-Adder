56940ec2665e46e2f0da5eefe6067775bbb4c17a
ChaoticGoose <raine@chaoticgoose.com>
2024-02-13 00:07:48 +1100

    Now runs recursively on filesystem

--- album-art-adder.py
+++ album-art-adder.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 import os
 from glob import glob
@@ -35,43 +35,47 @@ image_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff", ".webp"]
 
 
 def main():
-    directory = argv[1] if len(argv) > 1 else os.getcwd()
-    try:
-        # Get all files in directories with valid extentions
-        pattern = os.path.join(directory, "*%s")
-        try:
-            art = sum((glob(pattern % ext) for ext in image_extensions), [])[0]
-        except IndexError:
-            print(f"{bcolors.FAIL}Error: No image found in {directory}{bcolors.ENDC}")
-            return 1
-        files = sum((glob(pattern % ext) for ext in audio_extensions), [])
-        if not files:
-            print(
-                f"{bcolors.FAIL}Error: No audio files found in {directory}{bcolors.ENDC}"
-            )
-            return 1
+    selected_directory = argv[1] if len(argv) > 1 else os.getcwd()
+    sub_dirs = next(os.walk(selected_directory))[1]
+    sub_dirs.append(selected_directory)
 
-    except (NotADirectoryError, FileNotFoundError) as e:
-        print(f"{bcolors.FAIL}Error: {e}{bcolors.ENDC}")
-        print(
-            f"{bcolors.FAIL}Usage: python3 main.py {bcolors.UNDERLINE}<music-directory>{bcolors.ENDC} {bcolors.UNDERLINE}{bcolors.FAIL}<art-directory>{bcolors.ENDC}"
-        )
-        return 1
-
-    for file in files:
+    for directory in sub_dirs:
         try:
-            print(
-                f"{bcolors.OKGREEN}{'-'*os.get_terminal_size().columns}{bcolors.ENDC}"
-            )
-            print(f"{bcolors.OKCYAN}Processing: {file}{bcolors.ENDC}")
-            audio = music_tag.load_file(file)
-            with open(art, "rb") as img_in:
-                audio["artwork"] = img_in.read()  # Set the artwork
-            audio["artwork"].first.thumbnail([64, 64])  # Create a thumbnail
-            audio.save()
-            print(f"{bcolors.OKCYAN}Complete.{bcolors.ENDC}")
-        except Exception as e:
+            # Get all files in directories with valid extentions
+            pattern = os.path.join(directory, "*%s")
+            try:
+                art = sum((glob(pattern % ext) for ext in image_extensions), [])[0]
+            except IndexError:
+                print(f"{bcolors.WARNING}Warning: No image found in {directory}{bcolors.ENDC}")
+                continue
+            files = sum((glob(pattern % ext) for ext in audio_extensions), [])
+            if not files:
+                print(
+                    f"{bcolors.WARNING}Warning: No audio files found in {directory}{bcolors.ENDC}"
+                )
+                continue
+
+        except (NotADirectoryError, FileNotFoundError) as e:
             print(f"{bcolors.FAIL}Error: {e}{bcolors.ENDC}")
+            print(
+                f"{bcolors.FAIL}Usage: python3 main.py {bcolors.UNDERLINE}<music-directory>{bcolors.ENDC} {bcolors.UNDERLINE}{bcolors.FAIL}<art-directory>{bcolors.ENDC}"
+            )
+            return 1
+
+        for file in files:
+            try:
+                print(
+                    f"{bcolors.OKGREEN}{'-'*os.get_terminal_size().columns}{bcolors.ENDC}"
+                )
+                print(f"{bcolors.OKCYAN}Processing: {file}{bcolors.ENDC}")
+                audio = music_tag.load_file(file)
+                with open(art, "rb") as img_in:
+                    audio["artwork"] = img_in.read()  # Set the artwork
+                audio["artwork"].first.thumbnail([64, 64])  # Create a thumbnail
+                audio.save()
+                print(f"{bcolors.OKCYAN}Complete.{bcolors.ENDC}")
+            except Exception as e:
+                print(f"{bcolors.FAIL}Error: {e}{bcolors.ENDC}")
 
 
 if __name__ == "__main__":
